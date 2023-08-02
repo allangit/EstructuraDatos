537bfbcedf055d78b0bc48ebdca9153edea47109
descartes640@gmail.com <descartes640@gmail.com>
2023-08-01 18:04:11 -0600

    Ejercicio grafos Ultimo

--- Grafo.java
+++ Grafo.java
@@ -1,13 +1,23 @@
+
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+    Ejercicio grafos
+
+        1.implementar matriz de adyacencia
+        2.implemetar el metodo ingresar arista
+        3.implementar el metodo borrar arista
+        4.implementar el metodo verificar vecinos
+        5.implementar los metodos testing que verifique:
+
+            5.1 Agregar arista
+            5.2 verificar arista opuesta
+            5.3 borrar arista
+            Nota implementar las excepciones 
+
+
+*/
+
 package com.mycompany.practica;
 
-/**
- *
- * @author desca
- */
 public class Grafo {
     
 }
